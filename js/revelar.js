4597e9fff7cf42e5edc8a284b07592407dba5502
MAYCON <111540660+MAYCONARAUJONASCIMENTO@users.noreply.github.com>
2024-07-21 23:33:52 -0300

    Add files via upload

---
+++ js/revelar.js
@@ -0,0 +1,14 @@
+ScrollReveal().reveal('.menu,.fundo,.word,  ',
+    {
+        duration:3500,
+        distance: '80px',
+        
+    });
+    ScrollReveal().reveal('.txt-animation',
+        {
+            duration:3400,
+            distance: '70px',
+            
+        });
+
+    
